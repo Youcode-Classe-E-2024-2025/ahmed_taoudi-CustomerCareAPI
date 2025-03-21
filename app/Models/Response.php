552267a5ae46a2a545a6205e4719fadf326acca8
Tawdi <ahmedtawdi00@gmail.com>
2025-03-21 14:20:18 +0000

    ajoute fillable et orm relation pour Response

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Response extends Model
 {
-    //
+    protected $fillable = [
+        'ticket_id', 
+        'user_id',   //  agent
+        'response', 
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
