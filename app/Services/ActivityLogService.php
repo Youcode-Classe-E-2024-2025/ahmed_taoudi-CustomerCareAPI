9e7364bade464dd0cde42e673090eae5355175fd
Tawdi <ahmedtawdi00@gmail.com>
2025-03-21 14:50:38 +0000

    ajoute de activity log service

---
+++ app/Services/ActivityLogService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ActivityLog; 
+use Illuminate\Support\Facades\Auth;
+
+class ActivityLogService
+{
+    /**
+     * Log a new activity.
+     *
+     * @param string $action
+     * @param string $description
+     * @param string $subjectType
+     * @param string $subjectId
+     * @return \App\Models\ActivityLog
+     */
+    public function logActivity(string $action, string $description, string $subjectType, string $subjectId)
+    {
+
+        return ActivityLog::create([
+            'user_id' => Auth::id(), 
+            'action' => $action, 
+            'description' => $description, 
+            'subject_type' => $subjectType,
+            'subject_id' => $subjectId, 
+        ]);
+    }
+}
