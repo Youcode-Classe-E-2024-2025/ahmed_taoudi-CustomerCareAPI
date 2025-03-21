8e0cc79f3379a0afd8ebfb1b72c417bbfe20f990
Tawdi <ahmedtawdi00@gmail.com>
2025-03-21 14:19:57 +0000

    ajoute fillable et orm relation pour Ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status', // ('open', 'in_progress', 'closed')
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 }
