087efbecd36b857b8c23493b603dbf1b166f2540
Tawdi <ahmedtawdi00@gmail.com>
2025-03-23 21:08:35 +0000

    ajoute route responces

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +15,10 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+
+Route::apiResource('tickets',TicketController::class)->middleware('auth:sanctum');
+
+Route::resource('responses', ResponseController::class)->middleware('auth:sanctum');
