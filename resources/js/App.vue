a2c293213b5f7e29739661d970a164a4e19b1e1f
Tawdi <ahmedtawdi00@gmail.com>
2025-03-26 12:20:23 +0000

    ajoute de auth.vue et modifer app.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,11 @@
 <template>
     <NavBar></NavBar>
     <main>
-      <RouterView></RouterView>
+      <div class="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 flex justify-center items-center">
+
+          <RouterView></RouterView>
+
+      </div>
     </main>
 </template>
   
