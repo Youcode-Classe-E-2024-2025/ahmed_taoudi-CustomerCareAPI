0fe76589b750e8bfbbca78f3ac323eb10b79c9c8
Tawdi <ahmedtawdi00@gmail.com>
2025-03-26 11:19:31 +0000

    ajoute App.vue template global pou app

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <NavBar></NavBar>
+    <main>
+      <RouterView></RouterView>
+    </main>
+</template>
+  
+<script setup>
+import NavBar from './components/NavBar.vue';
+
+</script>
+  
+  <style></style>
